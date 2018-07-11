7a0d6383ff073ea85a5a058a2604332d9c285a98
PaurakhSharma <paurakh011@gmail.com>
2018-07-11 22:51:39 +0545

    add new file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const httpserver = require('http-server')
 
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
@@ -31,7 +32,27 @@ app.post('/post', (req, res) => {
 })
 
 app.get('/data', (req,res) => {
-    res.sendfile('./newfile.txt')
+    var query = url.parse(req.url, true).query;
+     
+if (typeof query.file === 'undefined') {
+    //specify Content will be an attachment
+    res.setHeader('Content-disposition', 'attachment; filename=theDocument.txt');
+    res.setHeader('Content-type', 'text/plain');
+    res.end("Hello, here is a file for you!");
+} else {
+    //read the image using fs and send the image content back in the response
+    fs.readFile('/' + query.file, function (err, content) {
+        if (err) {
+            res.writeHead(400, {'Content-type':'text/html'})
+            console.log(err);
+            res.end("No such file");    
+        } else {
+            //specify Content will be an attachment
+            res.setHeader('Content-disposition', 'attachment; filename='+query.file);
+            res.end(content);
+        }
+    });
+}
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
